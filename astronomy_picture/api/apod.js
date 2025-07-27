c3e5afeadcf81a48929f20c85f45fde5b0d3eb91
Lais Talita Brito de Oliveira <116667293+laisTalita@users.noreply.github.com>
2025-07-27 15:19:03 -0300

    teste

--- astronomy_picture/api/apod.js
+++ astronomy_picture/api/apod.js
@@ -1,14 +1,22 @@
-import fetch from 'node-fetch';
 export default async function handler(req, res) {
-  const  date  = req.query.date;
-  const apiKey = process.env.NASA_API_KEY; 
+  const date = req.query.date;
+  const apiKey = process.env.NASA_API_KEY;
 
-  const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${apiKey}`);
-
-  if (!response.ok) {
-     return res.status(response.status).json({ error: 'Erro ao buscar dados da NASA' });
-  
+  if (!date) {
+    return res.status(400).json({ error: 'Data é obrigatória' });
   }
-  const data = await response.json();
-  res.json(data);
+
+  try {
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${apiKey}`);
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: 'Erro ao buscar dados da NASA' });
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro interno no servidor' });
+  }
 }
