5912e1e42359a4e4b609282b3a31019068aa620a
Lais Talita Brito de Oliveira <116667293+laisTalita@users.noreply.github.com>
2025-10-20 15:13:03 -0300

    ok

--- astronomy_picture/api/apod.js
+++ astronomy_picture/api/apod.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default async function handler(req, res){
 
   const date = req.query.date;
 
@@ -8,10 +8,6 @@ export default async function handler(req, res) {
 
   try {
      const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.NASA_API_KEY}`);
-
-    if (!response.ok) {
-      return res.status(response.status).json({ error: 'Erro ao buscar dados da NASA' });
-    }
     const data = await response.json();
     res.json(data);
   } catch (error) {
